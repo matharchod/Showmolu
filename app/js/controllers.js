80e53c3c678b66e2e9c9d3aa1ef2381f670861bb
Jani Anderson <ja@janianderson.com>
2014-08-05 20:42:52 -0500

    New variables in controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -34,10 +34,12 @@ angular.module('myApp.controllers', [])
   }])
   .controller('dynamicBkgController', ['$scope', '$http', function($scope, $http)  {
     $scope.photos = Flickoluser.getSessionStorage(); //get sessionStorage
-    $scope.imageSet = '72157645741266837';
-    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=5ae3f6d6106e232dc531b19d44ccd668&photoset_id=' + $scope.imageSet + '&format=json&nojsoncallback=1').success(function(data) {
+    $scope.imageSet = '72157645741266837'; //flickr gallery ID
+    $scope.owner = '94139373@N05'; //your flickr ID
+    $scope.apiKey = '5ae3f6d6106e232dc531b19d44ccd668'; your flickr API key
+    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id=' + $scope.imageSet + '&format=json&nojsoncallback=1').success(function(data) {
       $scope.flickrData = data;
-      Flickolu($scope.flickrData);  // owner, imageSet      
+      Flickolu($scope.flickrData);  // create dynamic background       
     });
   }])
   .controller('portfolioController', ['$scope', function($scope) {
