a9d720e5970d8ae49fcbd60e2bbb369730c99793
Jani Anderson <ja@janianderson.com>
2014-08-05 20:55:02 -0500

    API key
    doesn’t work as variable yet

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -36,8 +36,8 @@ angular.module('myApp.controllers', [])
     $scope.photos = Flickoluser.getSessionStorage(); //get sessionStorage
     $scope.imageSet = '72157645741266837'; //flickr gallery ID
     $scope.owner = '94139373@N05'; //your flickr ID
-    $scope.apiKey = '5ae3f6d6106e232dc531b19d44ccd668'; your flickr API key
-    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id=' + $scope.imageSet + '&format=json&nojsoncallback=1').success(function(data) {
+    //$scope.apiKey = '5ae3f6d6106e232dc531b19d44ccd668'; your flickr API key
+    $http.get('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=5ae3f6d6106e232dc531b19d44ccd668&photoset_id=' + $scope.imageSet + '&format=json&nojsoncallback=1').success(function(data) {
       $scope.flickrData = data;
       Flickolu($scope.flickrData);  // create dynamic background       
     });
