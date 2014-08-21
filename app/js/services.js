e6c43603a4a93bf3f2834d3c7e4287477592bbf8
Jani Anderson <ja@janianderson.com>
2014-08-20 22:16:29 -0500

    Removing version directive

--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  factory('flickerGet', ['$resource',
+    function($resource){
+      return $resource('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=5ae3f6d6106e232dc531b19d44ccd668&photoset_id=72157645741266837&format=json&nojsoncallback=1');
+  }]);
